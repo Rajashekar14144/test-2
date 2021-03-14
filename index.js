7ff09bfc4691499ff940a7b51d6a35fb52dfab0a
Rajashekar813 <rshekar@pippintechnologies.com>
2021-03-14 22:16:19 +0530

    Added node mone

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
+    res.write('Hello World Raaaj');
     res.end();
 }).listen(3000);
