82b9f70082900fdf64ca96ba25e7b681eaaf544d
Rajashekar813 <rshekar@pippintitle.com>
2021-12-04 20:32:59 +0530

    Revert "From1 Raaj14"
    This reverts commit f6c73e0500d25fe0bc9fc4de2f786ce7c53c5b30.
    
    Revert needed

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ http.createServer(function (req, res) {
     res.write('Hello World Raaajshekar007 !!!!sdsds!');
     res.write('Hello World2 Raaajshekar007 !!!!sdsds!');
     
-    // From different developer
-
-    res.write('Hello World1 Raaajshekar14 !!!!sdsds!');
-
 
     
     res.write('Hello World Raaajshekar2  !!!!sdsds!');
